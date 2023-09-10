e9f2ca296ef64215d7952124b2aa36cfe859c240
leehj <leehj@localhost>
2023-09-10 20:37:38 +0900

    pom.xml check

--- koala-master/src/main/java/config/MvcConfig.java
+++ koala-master/src/main/java/config/MvcConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * DispatcherServlet은 웹 브라우저의 요청을 처리할 핸들러 객체를 찾기 위해 HandlerMapping을 사용하고 핸들러를 실행하기 위해 HandlerAdapter를 사용한다.
+ * 
+ */
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer{
@@ -15,8 +19,8 @@ public class MvcConfig implements WebMvcConfigurer{
 	//JSP를 위한 ViewResolver설정
 	/**
 	 * 컨트롤러의 실행 결과를 받은 DispatcherServlet은 ViewResolver에게 뷰 이름에 해당하는 View객체를 요청한다.
-	 *
-	 * **/
+	 * 
+	 */
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/",".jsp");
